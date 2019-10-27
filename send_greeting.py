3e29547c29e320507149bde0a8bb004e52ff20be
Yashraj Kakkad <yashrajkakkad@gmail.com>
2019-10-27 13:07:44 +0530

    Fixed Issue #3 Program stops working if a contact is not found

--- send_greeting.py
+++ send_greeting.py
@@ -12,16 +12,20 @@ contacts = dict()
 for key, value in wishingContacts.items():
     pre_msg = "Namaskar *"
     post_msg = "* Bit by bit, may every cracker cracked on the evening of Diwali bring light to your life, darkness to your IDE, sweet free shwags, gits of joy, colours of RGB, arrays of prosperity and drive away bugs of hopelessness from your life. May the time limit of your life be exceeded, the errors fixed and your joys increment multiflops. *Wish you a very Happy Diwali*"
-    input_box = driver.find_element_by_css_selector("input[type='text']")
-    input_box.click()
-    input_box.send_keys(key)
-    sleep(1)
-    userbox = driver.find_element_by_css_selector("span[title='"+key+"']")
-    userbox.click()
-    inputbox = driver.find_element_by_css_selector("div[data-tab='1']")
-
-    inputbox.click()
-    inputbox.send_keys(pre_msg+value+post_msg)
-    send_button = driver.find_element_by_css_selector("span[data-icon='send']")
-    send_button.click()
-    sleep(1)
+    input_box = None
+    try:
+        input_box = driver.find_element_by_css_selector("input[type='text']")
+        input_box.click()
+        input_box.send_keys(key)
+        sleep(1)
+        userbox = driver.find_element_by_css_selector("span[title='"+key+"']")
+        userbox.click()
+        inputbox = driver.find_element_by_css_selector("div[data-tab='1']")
+        inputbox.click()
+        inputbox.send_keys(pre_msg+value+post_msg)
+        send_button = driver.find_element_by_css_selector("span[data-icon='send']")
+        send_button.click()
+        sleep(1)
+    except Exception:
+        input_box.clear()
+        continue
