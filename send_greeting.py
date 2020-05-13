bf9f89a2d2565d30e418313b63f2f3c6db22d86c
Anubhav Mittal <anubhav11march@gmail.com>
2020-05-13 22:40:46 +0530

    :sparkles: Resolved Whatsapp web search update

--- send_greeting.py
+++ send_greeting.py
@@ -6,13 +6,13 @@ import filter_contacts
 wishingContacts = filter_contacts.filter_contacts("google.csv")
 driver = webdriver.Chrome('../../config/chromedriver')
 driver.get('https://web.whatsapp.com')
-sleep(15)
+sleep(30)
 contacts = dict()
 
 for key, value in wishingContacts.items():
     pre_msg = "Namaskar *"
     post_msg = "* Bit by bit, may every cracker cracked on the evening of Diwali bring light to your life, darkness to your IDE, sweet free shwags, gits of joy, colours of RGB, arrays of prosperity and drive away bugs of hopelessness from your life. May the time limit of your life be exceeded, the errors fixed and your joys increment multiflops. *Wish you a very Happy Diwali*"
-    input_box = driver.find_element_by_css_selector("input[type='text']")
+    input_box = driver.find_element_by_css_selector("div[data-tab='3']")
     input_box.click()
     input_box.send_keys(key)
     sleep(1)
